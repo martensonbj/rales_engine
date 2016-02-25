d71a2485c8cc8b88034d577b8c788fdbf2efef98
martensonbj <martensonbj@gmail.com>
2016-02-24 19:19:06 -0700

    Transactions controller test passing

---
+++ spec/controllers/api/v1/transactions/invoices_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::Transactions::InvoicesController, type: :controller do
+  describe "GET show" do
+    it "shows the associated item per invoice_item" do
+      transaction = create_transaction.first
+
+      get :show, format: :json, id: transaction.id
+
+      json_response = JSON.parse(response.body, symbolize_names: true)
+      assert_response :success
+      expect(json_response[:status]).to eq("shipped")
+    end
+  end
+
+end
